b5caf200fe1bd2e4ba2c52d4eb5138fc27b4024f
Jay Prakash Kalia <jaykalia047@gmail.com>
2024-02-09 02:56:56 +0530

    fix styles and finally solved the bug

--- src/lib/components/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-const Button = () => {
-   return (
-      <button onClick={()=>{console.log('chal jaa bhai')}} >
-        Click me 
-      </button>
-   )
-}
-export default Button;
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,3 +1,2 @@
 import  StarRating  from "./components/StarRating";
-import Button from './components/Button';
-export { StarRating, Button };
+export { StarRating };
