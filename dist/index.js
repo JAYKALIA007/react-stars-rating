6659b32506ac8ab7845d1c797b06554a5ef0927b
Jay Prakash Kalia <jaykalia047@gmail.com>
2024-02-09 02:57:08 +0530

    build

--- dist/index.js
+++ dist/index.js
@@ -3,12 +3,6 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-Object.defineProperty(exports, "Button", {
-  enumerable: true,
-  get: function get() {
-    return _Button.default;
-  }
-});
 Object.defineProperty(exports, "StarRating", {
   enumerable: true,
   get: function get() {
@@ -16,5 +10,4 @@ Object.defineProperty(exports, "StarRating", {
   }
 });
 var _StarRating = _interopRequireDefault(require("./components/StarRating"));
-var _Button = _interopRequireDefault(require("./components/Button"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
