a50346ebef69a3fcd4c07c5f877a34c4e500b349
Jay Prakash Kalia <jaykalia047@gmail.com>
2024-02-09 02:53:21 +0530

    build

--- dist/components/StarRating.js
+++ dist/components/StarRating.js
@@ -4,13 +4,14 @@ require("core-js/modules/es.weak-map.js");
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.default = StarRating;
+exports.default = void 0;
 require("core-js/modules/web.dom-collections.iterator.js");
 var _react = _interopRequireWildcard(require("react"));
-var _Star = require("./Star");
+var _Star = _interopRequireDefault(require("./Star"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 function _getRequireWildcardCache(e) { if ("function" != typeof WeakMap) return null; var r = new WeakMap(), t = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(e) { return e ? t : r; })(e); }
 function _interopRequireWildcard(e, r) { if (!r && e && e.__esModule) return e; if (null === e || "object" != typeof e && "function" != typeof e) return { default: e }; var t = _getRequireWildcardCache(r); if (t && t.has(e)) return t.get(e); var n = { __proto__: null }, a = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var u in e) if ("default" !== u && Object.prototype.hasOwnProperty.call(e, u)) { var i = a ? Object.getOwnPropertyDescriptor(e, u) : null; i && (i.get || i.set) ? Object.defineProperty(n, u, i) : n[u] = e[u]; } return n.default = e, t && t.set(e, n), n; }
-function StarRating() {
+const StarRating = () => {
   const [selectedStars, setSelectedStars] = (0, _react.useState)(1);
   const [hoveredStars, setHoveredStars] = (0, _react.useState)(0);
   return /*#__PURE__*/_react.default.createElement("div", null, Array(5).fill(null).map((_, index) => /*#__PURE__*/_react.default.createElement("span", {
@@ -18,7 +19,8 @@ function StarRating() {
     onClick: () => setSelectedStars(index + 1),
     onMouseOver: () => setHoveredStars(index + 1),
     onMouseLeave: () => setHoveredStars(0)
-  }, /*#__PURE__*/_react.default.createElement(_Star.Star, {
+  }, /*#__PURE__*/_react.default.createElement(_Star.default, {
     isFilled: hoveredStars > 0 ? index < hoveredStars : index < selectedStars
   }))));
-}
+};
+var _default = exports.default = StarRating;
--- dist/index.js
+++ dist/index.js
@@ -3,10 +3,18 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+Object.defineProperty(exports, "Button", {
+  enumerable: true,
+  get: function get() {
+    return _Button.default;
+  }
+});
 Object.defineProperty(exports, "StarRating", {
   enumerable: true,
   get: function get() {
-    return _StarRating.StarRating;
+    return _StarRating.default;
   }
 });
-var _StarRating = require("./components/StarRating");
+var _StarRating = _interopRequireDefault(require("./components/StarRating"));
+var _Button = _interopRequireDefault(require("./components/Button"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
